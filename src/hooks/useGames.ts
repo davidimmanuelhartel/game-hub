111a084c5e8180dc11d9264464aa970b0af11855
davidimmanuelhartel <david.immanuel.hartel@gmail.com>
2023-06-15 10:43:48 +0200

    create a generic data fetching hook

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,8 +1,4 @@
-import { useEffect, useState } from "react";
-import apiClent from "../services/api-clent";
-import { Cancel } from "@mui/icons-material";
-import { CanceledError } from "axios";
-import { NumberDecrementStepperProps } from "@chakra-ui/react";
+import useData from "./useData";
 
 export interface Platform {
     id: number;
@@ -17,36 +13,9 @@ export interface Game {
     parent_platforms: {platform: Platform}[] 
     metacritic: number;
   }
-  
-  interface FetchGamesResponse {
-    count: number;
-    results: Game[];
-  }
 
-const useGames = () => {
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState("");
-    const [isLoading, setLoading] = useState(false);
 
-    useEffect(() => {
-        const controller = new AbortController();
-        setLoading(true);
-      apiClent
-        .get<FetchGamesResponse>("/games", {signal: controller.signal})
-        .then((res) => {
-          setGames(res.data.results);
-          setLoading(false);
-        })
-        .catch((err) => { 
-            if (err instanceof CanceledError) return;
-            setError(err.message);
-            setLoading(false);
-        });
-    return () => controller.abort();
-    }, []);
-
-    return { games, error, isLoading};
-}
+const useGames = () => useData<Game>("/games");
 
 export default useGames;
 
